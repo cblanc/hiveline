be912f6b3bd53a67f1d2db60a8d8ba364b93610f
cblanc <cablanchard@gmail.com>
2015-01-29 17:59:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Hiveline
+
+Adjust your [Hive](https://my.hivehome.com/) thermostat via your terminal.
+
+Warning: This uses hives unofficial and undocumented API. It can break at any time.
+
+## Install
+
+```bash
+gem install hiveline
+```
+
+## Configure
+
+Export credentials to your environment in `.bashrc` or `.bash_profile`
+
+```bash
+export HIVE_USERNAME="your_hive_email"
+
+export HIVE_PASSWORD="your_hive_password"
+```
+
+Alternatively pass then as flags to hiveline
+
+```
+$ hiveline -u <email> -p <password> 
+```
+
+## Use
+
+```bash
+$ hiveline 20
+
+```
---
+++ index.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+require 'httparty'
+require'json'
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-u", "--username", "Login username") do |p|
+    options[:password] = v
+  end
+
+  opts.on("-p", "--password", "Login password") do |p|
+    options[:password] = p
+  end
+end.parse!
+
+
+username = options[:username] || ENV["HIVE_USERNAME"]
+
+if username.nil?
+	print "Please provide a username with a flag (-u) or as an environment variable exports HIVE_USERNAME=\"your_username\"\n"
+	exit false
+end
+
+password = options[:password] || ENV["HIVE_PASSWORD"]
+
+if password.nil?
+	print "Please provide a password with a flag (-p) or as an environment variable exports HIVE_PASSWORD=\"your_password\"\n"
+	exit false
+end
+
+# Login with credentials and return cookie
+
+def login(username, password)
+	print "Authenticating credentials...\n"
+	login_url = "https://my.hivehome.com/login"
+	HTTParty.post(login_url, {
+		body: {
+			username: username,
+			password: password
+		}, 
+		follow_redirects: false
+	})
+end
+
+def extract_id(response)
+	id_attribute_regex = /^hsid=/
+	set_cookie_header = response.headers["set-cookie"]
+	set_cookie_header
+		.split(";")
+		.each(&:strip!)
+		.grep(id_attribute_regex)
+		.first
+		.gsub(id_attribute_regex, "")
+end
+
+def get_id(username, password)
+	extract_id login(username, password)
+end
+
+def set_temperature(id, temp)
+	print "Setting temperature to #{temp}C...\n"
+	heating_url = "https://my.hivehome.com/heating/target"
+	response = HTTParty.put(heating_url, {
+		body: {
+			id:1,
+			target:temp
+		},
+		headers: {
+			"Cookie" => "hsid=#{id}"
+		},
+		follow_redirects: false
+	})
+
+	if response.code == 200
+		target = JSON.parse(response.body)["target"]
+		print "Successfully updated temperature. Set to #{target}C\n"
+	else
+		print "Something went wrong. Not sure what, maybe get up and go check your thermostat?\n"
+	end
+	response
+end
+
+id = get_id(username, password)
+set_temperature(id, 20)
